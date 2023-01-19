915b85fe3628d14b72e563d1e0e0c881aab34ae9
LautaroLesni <lautarolesniewicz@gmail.com>
2023-01-19 19:05:32 -0300

    initializated backend

---
+++ api/src/users/users.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Get, Body } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private userService: UsersService){}
+
+    @Get()
+    getUsers() :Promise<User[]>{
+        return this.userService.getUsers();
+    }
+
+    @Post()
+    createUser(@Body() newUser:CreateUserDto){
+      return this.userService.createUser(newUser)
+    }
+}
