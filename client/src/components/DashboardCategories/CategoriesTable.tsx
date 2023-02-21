77284fd8b532358282a7aab3110c569bbb0442e7
LautaroLesni <lautarolesniewicz@gmail.com>
2023-02-21 16:46:33 -0300

    add/frontend pagination to the categories admin dashboard

--- client/src/components/DashboardCategories/CategoriesTable.tsx
+++ client/src/components/DashboardCategories/CategoriesTable.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import Table from '@mui/material/Table';
+import IconButton from '@mui/material/IconButton';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
 import TableBody from '@mui/material/TableBody';
 import TableCell, {tableCellClasses} from '@mui/material/TableCell';
 import { styled } from '@mui/material/styles';
@@ -11,8 +18,78 @@ import s from './CategoriesTable.module.css'
 import { useCustomSelector, useCustomDispatch } from "../../hooks/hooks";
 import { useEffect } from "react";
 import { traerCategorias } from "../../redux/slices/categories";
+import TablePagination from '@mui/material/TablePagination';
+import TableFooter from '@mui/material/TableFooter';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    newPage: number,
+  ) => void;
+}
+
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+//React Component
 
 
 const CategoriesTable = () =>{
@@ -25,6 +102,30 @@ const CategoriesTable = () =>{
             dispatch(traerCategorias())
         }
     },[dispatch])
+
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows =
+      page > 0 ? Math.max(0, (1 + page) * rowsPerPage - categories!.length) : 0;
+  
+    const handleChangePage = (
+      event: React.MouseEvent<HTMLButtonElement> | null,
+      newPage: number,
+    ) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (
+      event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0);
+    };
+
+
+    // MUI Styles
 
     const StyledTableCell = styled(TableCell)(({ theme }) => ({
         [`&.${tableCellClasses.head}`]: {
@@ -46,7 +147,67 @@ const CategoriesTable = () =>{
         },
       }));
     return (
-        <TableContainer component={Paper}>
+      <TableContainer component={Paper}
+      style={{display:'flex', flexDirection:'column', alignItems:'center'}}>        
+              <h1 className={s.title}>Categorias</h1> 
+      <Table sx={{ minWidth: 650 }} aria-label="custom pagination table">
+        <TableHead>
+          <StyledTableRow>
+            <StyledTableCell>ID</StyledTableCell>
+            <StyledTableCell className={s.tableCell}>Name</StyledTableCell>
+            <StyledTableCell className={s.tableCell}>Edicion</StyledTableCell>
+          </StyledTableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? categories!.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : categories!
+          ).map((categories:any) => (
+            <StyledTableRow key={categories.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <StyledTableCell component="th" scope="row">
+                {categories.id}
+              </StyledTableCell>
+              <StyledTableCell align="left">
+                {categories.name.length > 20 ? `${categories.name.slice(0,20)}...` : `${categories.name}`}
+              </StyledTableCell>
+              <StyledTableCell align="left">
+                <EditIcon style={{margin:'5px', cursor:'pointer'}}/>
+                <DeleteIcon style={{margin:'5px', cursor:'pointer'}}/>
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+          {emptyRows > 0 && (
+            <StyledTableRow style={{ height: 53 * emptyRows }}>
+              <StyledTableCell colSpan={3} />
+            </StyledTableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <StyledTableRow>
+            <TablePagination
+              style={{backgroundColor:'#ffffff'}}
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={categories!.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </StyledTableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+
+
+/*         <TableContainer component={Paper}>
         <h1 className={s.title}>Categories</h1>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -70,7 +231,7 @@ const CategoriesTable = () =>{
             ))}
           </TableBody>
         </Table>
-      </TableContainer>
+      </TableContainer> */
     )
 }
 
---
+++ client/src/components/DashboardProducts/ProductsModalEdit.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ProductsModalEdit = () =>{
+    return (
+    <div>
+
+    </div>)
+}
+
+export default ProductsModalEdit
